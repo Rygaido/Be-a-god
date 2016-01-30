d358f08bf86fa49af44fe4dcf0b7e8ed63057871
Rygaido <rwg6766@GDD-31.main.ad.rit.edu>
2016-01-29 22:43:56 -0500

    Wandering Civilians

--- Assets/civilian_wander.cs
+++ Assets/civilian_wander.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class civilian_wander : MonoBehaviour {
 
-	float maxSpeed = 1;
-	float speed = 0;
+	float walkChance = 0.5f; //how likely an idle civilian is to start walking (1.0 is 100%, 0.0 is never)
 
-	float stateTimerMax = 1;
+	//the minimum and maximum speed an idle civilian may start moving at
+	float maxSpeed = 0.50f;
+	float minSpeed = 0.25f;
+
+	//the minimum and maximum lengths of time an idle civilian will spend doing one thing, then switches randomly to something else
+	float stateTimerMax = 2;
+	float stateTimerMin = 1;
+
+	//how far civilian can wander from center screen
+	float boundary = 3;
+
+	float speed = 0;
+	float direction = 0;
 	float stateTimer = 0;
 
-	float boundary = 1;
 
-	static Random r;
+	//static Random r = new Random();
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +30,44 @@ public class civilian_wander : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		speed = 0;
+		if (speed > 0.0f) {
+			transform.position += transform.forward * speed * Time.deltaTime; //apply movement
+			//transform.Translate(transform.forward * speed * Time.deltaTime); //apply movement
+		}
+		enforceBoundary ();
+
+		stateTimer -= Time.deltaTime; //decrement timer
+
+		//time up, choose a new actions
+		if (stateTimer <= 0) {
+			if(Random.Range(0.0f,1.0f) <= walkChance){ //walk in random direction (wander)
+				
+				speed = Random.Range (minSpeed, maxSpeed);
+				direction = Random.Range (0, 360);
+
+				transform.Rotate(Vector3.up, direction);
+			}
+			else{ //stand still
+				direction=0;
+				speed=0;
+			}
+
+			stateTimer = Random.Range(stateTimerMin,stateTimerMax);
+		}
+
+
+	}
+
+	//constrains civilians to a square of size "boundary" from origin in all directions
+	//sets civilians to move towards center
+	void enforceBoundary(){
+		if (Mathf.Abs(transform.position.x) > boundary || Mathf.Abs(transform.position.z) > boundary) {
+			Vector3 toCenter = -transform.position;
+			toCenter.y =0;
+			transform.rotation = Quaternion.LookRotation(toCenter, Vector3.up);
+			speed = minSpeed;
+			//transform.LookAt(Vector3.zero);
+			//transform.Rotate(transform.up, );
+		}
 	}
 }
